33088ddb6485e6980d930b39d1fc3c97a87b74c3
Rivaldo <ortegaalvarezrivaldo@gmail.com>
2022-02-03 10:50:19 -0500

    Mylittefriend backendr

--- routes/veterinaries.routes.js
+++ routes/veterinaries.routes.js
@@ -1,34 +1,35 @@
 const express = require('express');
-const { registerVeterinary } = require('../controllers/veterinary.controller');
-const { body } = require('express-validator')
+const {
+  registerVeterinary,
+  listVeterinaries,
+  findVeterinary,
+} = require('../controllers/veterinary.controller');
+const { body } = require('express-validator');
 const passport = require('passport');
-const validateJWT = passport.authenticate('jwt', { session:false, failWithError: true });
+const validateJWT = passport.authenticate('jwt', {
+  session: false,
+  failWithError: true,
+});
 
 const router = express.Router();
 
 /**
  * GET
  */
-router.get(
-    '/',
-    listVeterinaries
-);
-router.get(
-    '/:veterinaryId',
-    findVeterinary
-);
+router.get('/', listVeterinaries);
+router.get('/:veterinaryId', findVeterinary);
 
 /**
  * GET
  */
 router.post(
-    '/',
-    body('name').notEmpty(),
-    body('detail').optional({ checkFalsy: true }).isString(),
-    body('location').notEmpty(),
-    body('avatar_url').optional({ checkFalsy: true }).isString(),
-    body('services').isArray(),
-    registerVeterinary
+  '/',
+  body('name').notEmpty(),
+  body('detail').optional({ checkFalsy: true }).isString(),
+  body('location').notEmpty(),
+  body('avatar_url').optional({ checkFalsy: true }).isString(),
+  body('services').isArray(),
+  registerVeterinary
 );
 
 module.exports = router;
