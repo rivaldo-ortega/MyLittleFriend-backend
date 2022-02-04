78c5bf01be050f585d17d5455d8a166ff67a1597
Rivaldo <ortegaalvarezrivaldo@gmail.com>
2022-02-03 23:27:13 -0500

    Mylittefriend backend cors modified with specific port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const passport = require('passport');
 
 //cors
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header(
     'Access-Control-Allow-Headers',
     'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
