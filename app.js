68fd30a274050a851f154df27ca7cdf76d28df84
Rivaldo <ortegaalvarezrivaldo@gmail.com>
2022-02-03 23:20:15 -0500

    Mylittefriend backend cors m

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const { connectToDb } = require('./config/database');
 const passport = require('passport');
 
 //cors
-
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
